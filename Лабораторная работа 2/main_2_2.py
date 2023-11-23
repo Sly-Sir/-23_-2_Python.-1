5ad12853b26d3d05b8723060feb8a0c6873e5ca4
Sly-Sir <148986083+Sly-Sir@users.noreply.github.com>
2023-11-23 22:38:43 +0300

    Update main_2_2.py

--- Лабораторная работа 2/main_2_2.py
+++ Лабораторная работа 2/main_2_2.py
@@ -3,13 +3,11 @@ spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
 
-budjet = salary - spend
 money_capital = 0
+
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
-for _ in range(1, months):
-    budjet += salary
-    spend += spend * increase
-    budjet -= spend
-    money_capital = abs(budjet)
+for _ in range(months):
+    money_capital += spend - salary
+    spend *= 1 + increase
 
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital, 2))
